7f8dca78a42bb980c370f06696995d2c359dd1c8
long1 <duyquann123@gmail.com>
2022-10-30 09:30:12 +0700

    hhh

---
+++ app.js
@@ -0,0 +1,24 @@
+var sp1 = {
+    id: 1,
+    name: "Phuc Anh"
+}
+var sp2 = {
+    id: 2,
+    name: "TH"
+}
+var sp3 = {
+    id: 3,
+    name: "LC"
+}
+
+const sanphams = [sp1, sp2, sp3]
+
+for (let i = 0; i < sanphams.length; i++) {  
+}
+    // let i = 0
+    // while(i < sanphams.length){
+    //     // console.log(sanphams);
+    //     i++;
+    // }
+
+console.log (sanphams)
